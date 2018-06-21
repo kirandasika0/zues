7337ef99281bd0543520fdd94d4e177f7a3da60c
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-06-21 11:08:05 -0400

    updated client for deploy job rpc

--- client/client.go
+++ client/client.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"time"
 	pb "zues/proto"
+	"zues/util"
 
 	"github.com/kataras/golog"
 
@@ -12,23 +14,22 @@ import (
 )
 
 func main() {
-	for {
-		conn, err := grpc.Dial("localhost:8284", grpc.WithInsecure())
-		if err != nil {
-			golog.Error(err)
-		}
-		defer conn.Close()
-
-		c := pb.NewZuesControlClient(conn)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-		defer cancel()
-		res, err := c.GetInfo(ctx, &pb.Empty{})
-		if err != nil {
-			golog.Error(err)
-		}
-		golog.Info(fmt.Sprintf("%+v", res.K8SSession.ApiCalls))
-
-		time.Sleep(2 * time.Second)
+	conn, err := grpc.Dial("localhost:8284", grpc.WithInsecure())
+	if err != nil {
+		golog.Error(err)
 	}
+	defer conn.Close()
+
+	c := pb.NewZuesControlClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	content, _ := ioutil.ReadFile("../zues-config.yaml")
+	encodedBytes := util.EncodeBase64(content)
+	jobRequest := &pb.JobRequest{JobDescInYaml: encodedBytes, Timestamp: time.Now().Unix()}
+	res, err := c.DeployJob(ctx, jobRequest)
+	if err != nil {
+		golog.Error(err)
+	}
+	golog.Info(fmt.Sprintf("%+v", res))
 }
